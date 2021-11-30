a89ab8d71d7938580acaf3df9a0e0af49a8d4dfe
Brendan Allan <brendonovich@outlook.com>
2021-11-30 16:40:57 +0800

    correct webhook callback

--- lib/event_socket/twitch_api/helix/eventsub.ex
+++ lib/event_socket/twitch_api/helix/eventsub.ex
@@ -9,7 +9,7 @@ defmodule EventSocket.TwitchAPI.Helix.EventSub do
       "version" => 1,
       "transport" => %{
         "method" => "webhook",
-        "callback" => Env.self_origin() <> "/webhook",
+        "callback" => Env.self_origin() <> "/eventsub/webhook",
         "secret" => "1234567890"
       }
     })
